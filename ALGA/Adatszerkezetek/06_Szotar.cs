16c26abff8b348099ef559df98c99c88b0cdc64c
mtcsrht <mate.cserhati03@gmail.com>
2025-10-28 16:52:18 +0100

    feat: 6th week's assignment

--- ALGA/Adatszerkezetek/06_Szotar.cs
+++ ALGA/Adatszerkezetek/06_Szotar.cs
@@ -3,4 +3,119 @@
 namespace OE.ALGA.Adatszerkezetek
 {
     // 6. heti labor feladat - Tesztek: 06_SzotarTesztek.cs
+
+    public class SzotarElem<K, T>
+    {
+        public K kulcs;
+        public T tart;
+
+        public SzotarElem(K kulcs, T tart)
+        {
+            this.kulcs = kulcs;
+            this.tart = tart;
+        }
+    }
+    
+    public class HasitoSzotarTulcsordulasiTerulettel<K, T> : Szotar<K,T>
+    {
+        SzotarElem<K, T>[] E;
+
+        Func<K, int> h;
+        
+        LancoltLista<SzotarElem<K, T>> U;
+
+        public HasitoSzotarTulcsordulasiTerulettel(int meret, Func<K, int> hasitoFuggveny)
+        {
+            E = new SzotarElem<K, T>[meret];
+            U = new LancoltLista<SzotarElem<K, T>>();
+            h = (kulcs) => Math.Abs(hasitoFuggveny(kulcs)) % E.Length;
+        }
+
+        public HasitoSzotarTulcsordulasiTerulettel(int meret) : this(meret, (kulcs) => kulcs.GetHashCode())
+        {
+            
+        }
+
+        private SzotarElem<K, T> KulcsKeres(K kulcs)
+        {
+            int index = h(kulcs);
+            SzotarElem<K, T> elem = E[index];
+
+            if (elem != null && elem.kulcs.Equals(kulcs))
+            {
+                return elem;
+            }
+
+            foreach (SzotarElem<K,T> tulcsordultElem in U)
+            {
+                if (tulcsordultElem.kulcs.Equals(kulcs))
+                {
+                    return tulcsordultElem;
+                }
+            }
+            return null;
+        }
+        public void Beir(K kulcs, T ertek)
+        {
+            SzotarElem<K, T> letezoElem = KulcsKeres(kulcs);
+
+            if (letezoElem != null)
+            {
+                letezoElem.tart = ertek;
+            }
+            else
+            {
+                SzotarElem<K, T> ujElem = new SzotarElem<K, T>(kulcs, ertek);
+                int index = h(kulcs);
+
+                if (E[index] == null)
+                {
+                    E[index] = ujElem;
+                }
+                else
+                {
+                    U.Hozzafuz(ujElem);
+                }
+            }
+        }
+        public T Kiolvas(K kulcs)
+        {
+            SzotarElem<K, T> elem = KulcsKeres(kulcs);
+            if (elem != null)
+            {
+                return elem.tart;
+            }
+            throw new HibasKulcsKivetel();
+        }
+        public void Torol(K kulcs)
+        {
+            int index = h(kulcs);
+            SzotarElem<K, T> elemE = E[index];
+            
+            if (elemE != null && elemE.kulcs.Equals(kulcs))
+            {
+                E[index] = null;
+                return;
+            }
+            
+            SzotarElem<K, T> torlendoElem = null;
+            foreach (SzotarElem<K, T> elemU in U)
+            {
+                if (elemU.kulcs.Equals(kulcs))
+                {
+                    torlendoElem = elemU;
+                    break;
+                }
+            }
+            
+            if (torlendoElem != null)
+            {
+
+                U.Torol(torlendoElem);
+                return;
+            }
+            
+            throw new HibasKulcsKivetel();
+        }
+    }
 }
--- Tesztek/06_SzotarTesztek.cs
+++ Tesztek/06_SzotarTesztek.cs
@@ -1,112 +1,112 @@
-﻿//using NUnit.Framework;
-//using OE.ALGA.Adatszerkezetek;
+﻿using NUnit.Framework;
+using OE.ALGA.Adatszerkezetek;
 
-//namespace OE.ALGA.Tesztek.Adatszerkezetek
-//{
-//    [TestFixture(Category = "Adatszerkezetek", TestName = "06 - Szótár Tesztek")]
-//    public class SzotarTesztek
-//    {
-//        private static int TesztHasitoFuggveny(string kulcs)
-//        {
-//            if (string.IsNullOrEmpty(kulcs))
-//                return 0;
-//            int sum = 0;
-//            foreach (char c in kulcs.ToCharArray())
-//                sum += ((byte)c);
-//            return sum * sum; // a modulo osztást a szótárnak kell végeznie, mert ő tudja csak a belső tömb méretet
-//        }
+namespace OE.ALGA.Tesztek.Adatszerkezetek
+{
+    [TestFixture(Category = "Adatszerkezetek", TestName = "06 - Szótár Tesztek")]
+    public class SzotarTesztek
+    {
+        private static int TesztHasitoFuggveny(string kulcs)
+        {
+            if (string.IsNullOrEmpty(kulcs))
+                return 0;
+            int sum = 0;
+            foreach (char c in kulcs.ToCharArray())
+                sum += ((byte)c);
+            return sum * sum; // a modulo osztást a szótárnak kell végeznie, mert ő tudja csak a belső tömb méretet
+        }
 
-//        [TestCase]
-//        public void AlapMukodes()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
-//                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
-//            });
-//        }
-//        [TestCase]
-//        public void AlapertelmezettHasitoFuggvennyel() //F2.(f)
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
-//                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
-//            });
-//        }
-//        [TestCase]
-//        public void Kulcsutkozes()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Bale", 15);
-//            sz.Beir("Lajos", 2);
-//            sz.Beir("Lasoj", 12);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
-//                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
-//                Assert.That(sz.Kiolvas("Bale"), Is.EqualTo(15));
-//                Assert.That(sz.Kiolvas("Lasoj"), Is.EqualTo(12));
-//            });
-//        }
-//        [TestCase]
-//        public void NullElem()
-//        {
-//            Szotar<string, string> sz = new HasitoSzotarTulcsordulasiTerulettel<string, string>(5, TesztHasitoFuggveny);
-//            sz.Beir("null", null!);
-//        }
-//        [TestCase]
-//        public void UresKulcs()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
-//            sz.Beir("", 0);
-//        }
-//        [TestCase]
-//        public void UresElem()
-//        {
-//            Szotar<string, string> sz = new HasitoSzotarTulcsordulasiTerulettel<string, string>(5, TesztHasitoFuggveny);
-//            sz.Beir("Bela", "");
-//        }
-//        [TestCase]
-//        public void NincsElem()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            Assert.Throws<HibasKulcsKivetel>(() => sz.Kiolvas("Ferenc"));
-//        }
-//        [TestCase]
-//        public void TorlesNull()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            Assert.Throws<HibasKulcsKivetel>(() => sz.Torol(null!));
-//        }
-//        [TestCase]
-//        public void TorlesMarad()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            sz.Torol("Bela");
-//            Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
-//        }
-//        [TestCase]
-//        public void TorlesEltunt()
-//        {
-//            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
-//            sz.Beir("Bela", 5);
-//            sz.Beir("Lajos", 2);
-//            sz.Torol("Bela");
-//            Assert.Throws<HibasKulcsKivetel>(() => sz.Kiolvas("Bela"));
-//        }
-//    }
-//}
+        [TestCase]
+        public void AlapMukodes()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            Assert.Multiple(() =>
+            {
+                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
+                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
+            });
+        }
+        [TestCase]
+        public void AlapertelmezettHasitoFuggvennyel() //F2.(f)
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            Assert.Multiple(() =>
+            {
+                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
+                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
+            });
+        }
+        [TestCase]
+        public void Kulcsutkozes()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(10, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Bale", 15);
+            sz.Beir("Lajos", 2);
+            sz.Beir("Lasoj", 12);
+            Assert.Multiple(() =>
+            {
+                Assert.That(sz.Kiolvas("Bela"), Is.EqualTo(5));
+                Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
+                Assert.That(sz.Kiolvas("Bale"), Is.EqualTo(15));
+                Assert.That(sz.Kiolvas("Lasoj"), Is.EqualTo(12));
+            });
+        }
+        [TestCase]
+        public void NullElem()
+        {
+            Szotar<string, string> sz = new HasitoSzotarTulcsordulasiTerulettel<string, string>(5, TesztHasitoFuggveny);
+            sz.Beir("null", null!);
+        }
+        [TestCase]
+        public void UresKulcs()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
+            sz.Beir("", 0);
+        }
+        [TestCase]
+        public void UresElem()
+        {
+            Szotar<string, string> sz = new HasitoSzotarTulcsordulasiTerulettel<string, string>(5, TesztHasitoFuggveny);
+            sz.Beir("Bela", "");
+        }
+        [TestCase]
+        public void NincsElem()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            Assert.Throws<HibasKulcsKivetel>(() => sz.Kiolvas("Ferenc"));
+        }
+        [TestCase]
+        public void TorlesNull()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            Assert.Throws<HibasKulcsKivetel>(() => sz.Torol(null!));
+        }
+        [TestCase]
+        public void TorlesMarad()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            sz.Torol("Bela");
+            Assert.That(sz.Kiolvas("Lajos"), Is.EqualTo(2));
+        }
+        [TestCase]
+        public void TorlesEltunt()
+        {
+            Szotar<string, int> sz = new HasitoSzotarTulcsordulasiTerulettel<string, int>(5, TesztHasitoFuggveny);
+            sz.Beir("Bela", 5);
+            sz.Beir("Lajos", 2);
+            sz.Torol("Bela");
+            Assert.Throws<HibasKulcsKivetel>(() => sz.Kiolvas("Bela"));
+        }
+    }
+}
