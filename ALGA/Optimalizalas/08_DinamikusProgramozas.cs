8ea6f72d9e95d9aaaa9a190524f66efd805776c7
Máté Cserháti <mate.cserhati03@gmail.com>
2025-11-09 23:03:46 +0100

    feat: 8th week assignments

--- ALGA/Optimalizalas/08_DinamikusProgramozas.cs
+++ ALGA/Optimalizalas/08_DinamikusProgramozas.cs
@@ -1,4 +1,113 @@
-﻿namespace OE.ALGA.Optimalizalas
+﻿using System;
+
+namespace OE.ALGA.Optimalizalas
 {
     // 8. heti labor feladat - Tesztek: 08_DinamikusProgramozasTesztek.cs
+    public class DinamikusHatizsakPakolas
+    {
+        private HatizsakProblema problema;
+        private double[,] F_cache = null;
+        public int LepesSzam { get; private set; }
+        
+        public DinamikusHatizsakPakolas(HatizsakProblema problema)
+        {
+            this.problema = problema; 
+            this.LepesSzam = 0;
+        }
+        public bool[] OptimalisMegoldas()
+        {
+            double[,] F = TablazatFeltoltes();
+
+            bool[] pakolas = new bool[problema.n]; 
+            int n = problema.n;
+            int Wmax = problema.Wmax;
+            
+            int t = n;
+            int h = Wmax;
+
+            while (t > 0 && h > 0)
+            {
+                int itemIndex = t - 1;
+                
+                if (F[t, h] > F[t - 1, h])
+                {
+                    pakolas[itemIndex] = true;
+                    h -= problema.w[itemIndex];
+                }
+                else
+                {
+                    pakolas[itemIndex] = false;
+                }
+                
+                t--;
+            }
+            
+            return pakolas;
+        }
+        public double OptimalisErtek()
+        {
+            double[,] F = TablazatFeltoltes();
+            
+            return F[problema.n, problema.Wmax];
+        }
+        public double[,] TablazatFeltoltes()
+        {
+            
+            if (F_cache != null)
+            {
+                return F_cache;
+            }
+
+            int n = problema.n;
+            int Wmax = problema.Wmax;
+
+            
+            double[,] F = new double[n + 1, Wmax + 1];
+            
+            LepesSzam = 0;
+
+            
+            for (int t = 0; t <= n; t++)
+            {
+                
+                for (int h = 0; h <= Wmax; h++)
+                {
+                    
+                    if (t == 0 || h == 0)
+                    {
+                        F[t, h] = 0;
+                    }
+                    else
+                    {
+                        
+                        int itemIndex = t - 1;
+                        int itemWeight = problema.w[itemIndex];
+                        double itemValue = problema.p[itemIndex];
+
+                        
+                        LepesSzam++; 
+
+                        
+                        if (h < itemWeight)
+                        {
+                            
+                            F[t, h] = F[t - 1, h];
+                        }
+                        
+                        else
+                        {
+                            
+                            double ertek_nem_visszuk = F[t - 1, h];
+                            double ertek_visszuk = F[t - 1, h - itemWeight] + itemValue;
+                            
+                            F[t, h] = Math.Max(ertek_nem_visszuk, ertek_visszuk);
+                        }
+                    }
+                }
+            }
+
+            F_cache = F;
+            return F_cache;
+        }
+    }
 }
--- Tesztek/08_DinamikusProgramozasTesztek.cs
+++ Tesztek/08_DinamikusProgramozasTesztek.cs
@@ -1,67 +1,67 @@
-﻿//using NUnit.Framework;
-//using System;
-//using OE.ALGA.Optimalizalas;
+﻿using NUnit.Framework;
+using System;
+using OE.ALGA.Optimalizalas;
 
-//namespace OE.ALGA.Tesztek.Optimalizalas
-//{
-//    [TestFixture(Category = "Optimalizalas", TestName = "08 - Dinamikus Programozás Tesztek")]
-//    public class DinamikusProgramozasTesztek
-//    {
-//        [TestCase]
-//        public void UresTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(0, 0, [], []);
-//            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(0));
-//                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(Array.Empty<bool>()));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(0));
-//            });
-//        }
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaErtekTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(24));
-//            });
-//        }
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(24));
-//            });
-//        }
-//        [TestCase]
-//        public void NagyPeldaMegoldasErtekTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(1700));
-//            });
-//        }
-//        [TestCase]
-//        public void NagyPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.nagy_optimalis_pakolas));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(1700));
-//            });
-//        }
-//    }
-//}
+namespace OE.ALGA.Tesztek.Optimalizalas
+{
+    [TestFixture(Category = "Optimalizalas", TestName = "08 - Dinamikus Programozás Tesztek")]
+    public class DinamikusProgramozasTesztek
+    {
+        [TestCase]
+        public void UresTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(0, 0, [], []);
+            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(0));
+                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(Array.Empty<bool>()));
+                Assert.That(opt.LepesSzam, Is.EqualTo(0));
+            });
+        }
+        [TestCase]
+        public void JegyzetbenLevoPeldaErtekTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(24));
+            });
+        }
+        [TestCase]
+        public void JegyzetbenLevoPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
+                Assert.That(opt.LepesSzam, Is.EqualTo(24));
+            });
+        }
+        [TestCase]
+        public void NagyPeldaMegoldasErtekTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(1700));
+            });
+        }
+        [TestCase]
+        public void NagyPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            DinamikusHatizsakPakolas opt = new DinamikusHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.nagy_optimalis_pakolas));
+                Assert.That(opt.LepesSzam, Is.EqualTo(1700));
+            });
+        }
+    }
+}
