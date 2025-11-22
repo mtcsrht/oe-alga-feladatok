5af83732d25dc7a5252767a7ce573fc2bf00c360
Máté Cserháti <mate.cserhati03@gmail.com>
2025-11-22 10:54:48 +0100

    feat: week 9 assignment

--- ALGA/Optimalizalas/09_VisszalepesesKereses.cs
+++ ALGA/Optimalizalas/09_VisszalepesesKereses.cs
@@ -2,5 +2,319 @@
 
 namespace OE.ALGA.Optimalizalas
 {
-    // 9. heti labor feladat - Tesztek: 09VisszalepesesKeresesTesztek.cs
+    public class VisszalepesesOptimalizacio<T>
+    {
+        protected int n;
+        protected int[] M;
+        protected T[,] R;
+        protected Func<int, T, bool> ft;
+        protected Func<int, T, T[], bool> fk;
+        protected Func<T[], double> josag;
+        
+        public int LepesSzam { get; protected set; }
+        
+        public VisszalepesesOptimalizacio(
+            int n,
+            int[] M,
+            T[,] R,
+            Func<int, T, bool> ft,
+            Func<int, T, T[], bool> fk,
+            Func<T[], double> josag)
+        {
+            this.n = n;
+            this.M = M;
+            this.R = R;
+            this.ft = ft;
+            this.fk = fk;
+            this.josag = josag;
+            this.LepesSzam = 0;
+        }
+        
+        protected void Backtrack(int szint, T[] E, ref bool van, ref T[] O)
+        {
+            if (szint == n)
+            {
+                LepesSzam++;
+                double ertEk = josag(E);
+                if (!van || ertEk > josag(O))
+                {
+                    van = true;
+                    Array.Copy(E, O, n);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < M[szint]; i++)
+                {
+                    LepesSzam++;
+                    T r = R[szint, i];
+                    if (ft(szint, r) && fk(szint, r, E))
+                    {
+                        E[szint] = r;
+                        Backtrack(szint + 1, E, ref van, ref O);
+                    }
+                }
+            }
+        }
+        
+        public T[] OptimalisMegoldas()
+        {
+            T[] E = new T[n];
+            T[] O = new T[n];
+            bool van = false;
+            
+            Backtrack(0, E, ref van, ref O);
+            
+            return van ? O : null;
+        }
+    }
+    
+    public class VisszalepesesHatizsakPakolas
+    {
+        protected HatizsakProblema problema;
+        
+        public int LepesSzam { get; protected set; }
+        
+        public VisszalepesesHatizsakPakolas(HatizsakProblema problema)
+        {
+            this.problema = problema;
+        }
+        
+        public virtual bool[] OptimalisMegoldas()
+        {
+            int n = problema.n;
+            
+            int[] M = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                M[i] = 2;
+            }
+            
+            bool[,] R = new bool[n, 2];
+            for (int i = 0; i < n; i++)
+            {
+                R[i, 0] = true;
+                R[i, 1] = false;
+            }
+            
+            Func<int, bool, bool> ft = (szint, r) => true;
+            
+            Func<int, bool, bool[], bool> fk = (szint, r, E) =>
+            {
+                
+                int osszSuly = 0;
+                for (int i = 0; i < szint; i++)
+                {
+                    if (E[i])
+                    {
+                        osszSuly += problema.w[i];
+                    }
+                }
+                osszSuly += problema.w[szint];
+                
+                return osszSuly <= problema.Wmax;
+            };
+            
+            Func<bool[], double> josag = (E) =>
+            {
+                double osszErtek = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (E[i])
+                    {
+                        osszErtek += problema.p[i];
+                    }
+                }
+                return osszErtek;
+            };
+            
+            var optimalizalo = new VisszalepesesOptimalizacio<bool>(
+                n, M, R, ft, fk, josag);
+            
+            bool[] eredmeny = optimalizalo.OptimalisMegoldas();
+            this.LepesSzam = optimalizalo.LepesSzam;
+            
+            return eredmeny;
+        }
+        
+        public double OptimalisErtek()
+        {
+            bool[] megoldas = OptimalisMegoldas();
+            if (megoldas == null) return 0;
+            
+            double osszErtek = 0;
+            for (int i = 0; i < megoldas.Length; i++)
+            {
+                if (megoldas[i])
+                {
+                    osszErtek += problema.p[i];
+                }
+            }
+            return osszErtek;
+        }
+    }
+    
+    public class SzetvalasztasEsKorlatozasOptimalizacio<T> : VisszalepesesOptimalizacio<T>
+    {
+        protected Func<int, T[], double> fb;
+        
+        public SzetvalasztasEsKorlatozasOptimalizacio(
+            int n,
+            int[] M,
+            T[,] R,
+            Func<int, T, bool> ft,
+            Func<int, T, T[], bool> fk,
+            Func<T[], double> josag,
+            Func<int, T[], double> fb)
+            : base(n, M, R, ft, fk, josag)
+        {
+            this.fb = fb;
+        }
+        
+        protected new void Backtrack(int szint, T[] E, ref bool van, ref T[] O)
+        {
+            LepesSzam++;
+            
+            if (szint == n)
+            {
+                double ertEk = josag(E);
+                if (!van || ertEk > josag(O))
+                {
+                    van = true;
+                    Array.Copy(E, O, n);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < M[szint]; i++)
+                {
+                    T r = R[szint, i];
+                    if (ft(szint, r) && fk(szint, r, E))
+                    {
+                        E[szint] = r;
+                        
+                        double aktualisErtek = josag(E);
+                        double becsles = aktualisErtek + fb(szint + 1, E);
+                        
+                        if (!van || becsles > josag(O))
+                        {
+                            Backtrack(szint + 1, E, ref van, ref O);
+                        }
+                    }
+                }
+            }
+        }
+        
+        public new T[] OptimalisMegoldas()
+        {
+            T[] E = new T[n];
+            T[] O = new T[n];
+            bool van = false;
+            
+            Backtrack(0, E, ref van, ref O);
+            
+            return van ? O : null;
+        }
+    }
+    
+    public class SzetvalasztasEsKorlatozasHatizsakPakolas : VisszalepesesHatizsakPakolas
+    {
+        public SzetvalasztasEsKorlatozasHatizsakPakolas(HatizsakProblema problema)
+            : base(problema)
+        {
+        }
+        
+        public override bool[] OptimalisMegoldas()
+        {
+            int n = problema.n;
+            
+            int[] M = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                M[i] = 2;
+            }
+            
+            bool[,] R = new bool[n, 2];
+            for (int i = 0; i < n; i++)
+            {
+                R[i, 0] = true;
+                R[i, 1] = false;
+            }
+            
+
+            Func<int, bool, bool> ft = (szint, r) => true;
+            
+
+            Func<int, bool, bool[], bool> fk = (szint, r, E) =>
+            {
+                if (!r) return true;
+                
+                int osszSuly = 0;
+                for (int i = 0; i < szint; i++)
+                {
+                    if (E[i])
+                    {
+                        osszSuly += problema.w[i];
+                    }
+                }
+                osszSuly += problema.w[szint];
+                
+                return osszSuly <= problema.Wmax;
+            };
+
+            
+            Func<bool[], double> josag = (E) =>
+            {
+                double osszErtek = 0;
+                for (int i = 0; i < E.Length; i++)
+                {
+                    if (E[i])
+                    {
+                        osszErtek += problema.p[i];
+                    }
+                }
+                return osszErtek;
+            };
+            
+
+            Func<int, bool[], double> fb = (szint, E) =>
+            {
+                if (szint >= n) return 0;
+                
+                int hAtralevoKapacitas = problema.Wmax;
+                for (int i = 0; i < szint; i++)
+                {
+                    if (E[i])
+                    {
+                        hAtralevoKapacitas -= problema.w[i];
+                    }
+                }
+                
+                double becsles = 0;
+                for (int i = szint; i < n; i++)
+                {
+                    if (problema.w[i] <= hAtralevoKapacitas)
+                    {
+                        becsles += problema.p[i];
+                        hAtralevoKapacitas -= problema.w[i];
+                    }
+                    else
+                    {
+                        becsles += ((double)hAtralevoKapacitas / problema.w[i]) * problema.p[i];
+                        break;
+                    }
+                }
+                
+                return becsles;
+            };
+            
+            var optimalizalo = new SzetvalasztasEsKorlatozasOptimalizacio<bool>(
+                n, M, R, ft, fk, josag, fb);
+            
+            bool[] eredmeny = optimalizalo.OptimalisMegoldas();
+            this.LepesSzam = optimalizalo.LepesSzam;
+            
+            return eredmeny;
+        }
+    }
 }
--- Tesztek/09_VisszalepesesKereses.cs
+++ Tesztek/09_VisszalepesesKereses.cs
@@ -1,103 +1,103 @@
-﻿//using NUnit.Framework;
-//using OE.ALGA.Optimalizalas;
+﻿using NUnit.Framework;
+using OE.ALGA.Optimalizalas;
 
-//namespace OE.ALGA.Tesztek.Optimalizalas
-//{
-//    [TestFixture(Category = "Optimalizalas", TestName = "09 - Visszalépéses Keresés Tesztek")]
-//    public class VisszalepesesKeresesTesztek
-//    {
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaErtekTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(98));
-//            });
-//        }
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(98));
-//            });
-//        }
-//        [TestCase]
-//        public void NagyPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(4832));
-//            });
-//        }
-//        [TestCase]
-//        public void LepesszamVizsgalat()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(4832));
-//            });
-//        }
-//    }
+namespace OE.ALGA.Tesztek.Optimalizalas
+{
+    [TestFixture(Category = "Optimalizalas", TestName = "09 - Visszalépéses Keresés Tesztek")]
+    public class VisszalepesesKeresesTesztek
+    {
+        [TestCase]
+        public void JegyzetbenLevoPeldaErtekTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
+                    Assert.That(opt.LepesSzam, Is.EqualTo(98));
+            });
+        }
+        [TestCase]
+        public void JegyzetbenLevoPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
+                Assert.That(opt.LepesSzam, Is.EqualTo(98));
+            });
+        }
+        [TestCase]
+        public void NagyPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(4832));
+            });
+        }
+        [TestCase]
+        public void LepesszamVizsgalat()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            VisszalepesesHatizsakPakolas opt = new VisszalepesesHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(4832));
+            });
+        }
+    }
 
-//    [TestFixture(Category = "Optimalizalas", TestName = "09 - Szétválasztás és Korlátozás Tesztek")]
-//    public class SzetvalasztasEsKorlatozasTesztek
-//    {
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaErtekTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(62));
-//            });
-//        }
-//        [TestCase]
-//        public void JegyzetbenLevoPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
-//            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(62));
-//            });
-//        }
-//        [TestCase]
-//        public void NagyPeldaMegoldasTeszt()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(618));
-//            });
-//        }
-//        [TestCase]
-//        public void LepesszamVizsgalat()
-//        {
-//            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
-//            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
-//            Assert.Multiple(() =>
-//            {
-//                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
-//                Assert.That(opt.LepesSzam, Is.EqualTo(618));
-//            });
-//        }
-//    }
-//}
+    [TestFixture(Category = "Optimalizalas", TestName = "09 - Szétválasztás és Korlátozás Tesztek")]
+    public class SzetvalasztasEsKorlatozasTesztek
+    {
+        [TestCase]
+        public void JegyzetbenLevoPeldaErtekTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.jegyzet_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(62));
+            });
+        }
+        [TestCase]
+        public void JegyzetbenLevoPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.jegyzet_n, PakolasTesztEsetek.jegyzet_Wmax, PakolasTesztEsetek.jegyzet_w, PakolasTesztEsetek.jegyzet_p);
+            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisMegoldas(), Is.EquivalentTo(PakolasTesztEsetek.jegyzet_optimalis_pakolas));
+                Assert.That(opt.LepesSzam, Is.EqualTo(62));
+            });
+        }
+        [TestCase]
+        public void NagyPeldaMegoldasTeszt()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(618));
+            });
+        }
+        [TestCase]
+        public void LepesszamVizsgalat()
+        {
+            HatizsakProblema problema = new HatizsakProblema(PakolasTesztEsetek.nagy_n, PakolasTesztEsetek.nagy_Wmax, PakolasTesztEsetek.nagy_w, PakolasTesztEsetek.nagy_p);
+            SzetvalasztasEsKorlatozasHatizsakPakolas opt = new SzetvalasztasEsKorlatozasHatizsakPakolas(problema);
+            Assert.Multiple(() =>
+            {
+                Assert.That(opt.OptimalisErtek(), Is.EqualTo(PakolasTesztEsetek.nagy_optimalis_ertek));
+                Assert.That(opt.LepesSzam, Is.EqualTo(618));
+            });
+        }
+    }
+}
